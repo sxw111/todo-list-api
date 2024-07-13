9bbf77ba95cd71e691de1d538a21f23d87cda71a
sxw111 <sxw9sxw@gmail.com>
2024-07-13 18:41:05 +0300

    Add middleware and config vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,36 @@
 from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import decouple
 
 
 class Settings(BaseSettings):
+    TITLE: str = "Todo list application"
+    VERSION: str = "0.1.0"
+    TIMEZONE: str = "UTC"
+    DESCRIPTION: str | None = None
+    DEBUG: bool = False
+
     DATABASE_URL: PostgresDsn
+
     ACCESS_SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_SECRET_KEY: str
     REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    IS_ALLOWED_CREDENTIALS: bool = decouple.config("IS_ALLOWED_CREDENTIALS", cast=bool)
+    ALLOWED_ORIGINS: list[str] = [
+        "http://localhost:3000",
+        "http://0.0.0.0:3000",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
+        "http://localhost:5173",
+        "http://0.0.0.0:5173",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ]
+    ALLOWED_METHODS: list[str] = ["*"]
+    ALLOWED_HEADERS: list[str] = ["*"]
 
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
 from app.api.endpoints import api_router
+from starlette.middleware.cors import CORSMiddleware
 
+from app.core.config import settings
 
 app = FastAPI()
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=settings.IS_ALLOWED_CREDENTIALS,
+    allow_methods=settings.ALLOWED_METHODS,
+    allow_headers=settings.ALLOWED_HEADERS,
+)
 
 
 @app.get("/healthcheck", include_in_schema=False)
